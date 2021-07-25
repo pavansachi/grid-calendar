63f1241068a714e2492838fd591eab4e8a4b0abf
Pavan Sachidananda <pavansachi@192-168-1-112.tpgi.com.au>
2021-07-25 13:37:50 +1000

    add js

---
+++ main.js
@@ -0,0 +1,18 @@
+// const currentMonthDates = Array.from({length: moment().daysInMonth()}, (x, i) => moment().startOf('month').add(i, 'days'));
+
+// // alert(currentMonthDates);
+
+// let day = document.createElement('div');
+// day.classList.add('day');
+
+// let date = document.createElement('div');
+// date.classList.add('date');
+
+// day.appendChild(date)
+
+// var one = document.createTextNode("1"); 
+
+// date.appendChild(one);
+// console.log(day)
+
+// document.querySelector('.calendar').appendChild(day)
